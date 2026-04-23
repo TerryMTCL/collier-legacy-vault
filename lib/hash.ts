22193f42fb2123dba9d7c9cedb1291626f8cbe3b
TerryMTCL <263766744+TerryMTCL@users.noreply.github.com>
2026-04-23 01:07:59 -0500

    Fix: TypeScript Uint8Array buffer type for PBKDF2 salt

--- lib/hash.ts
+++ lib/hash.ts
@@ -12,7 +12,7 @@ async function deriveKey(password: string, salt: Uint8Array): Promise<ArrayBuffe
   return crypto.subtle.deriveBits(
     {
       name: 'PBKDF2',
-      salt,
+      salt: salt.buffer as ArrayBuffer,
       iterations: 100000,
       hash: 'SHA-256',
     },
